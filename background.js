dd65f488d5238096adbb496bc4a2d89c50184fd5
FabianDotCom <fabian.rhcp@gmail.com>
2019-01-25 05:35:17 -0600

    Improved new window size

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.alarms.onAlarm.addListener(function(alarma) {
-  chrome.windows.create({ url: 'index.html', type: 'popup' });
+  chrome.windows.create({
+    url: 'index.html',
+    type: 'popup',
+    width: 400,
+    height: 200,
+    left: 300,
+    top: 350
+  });
 });
